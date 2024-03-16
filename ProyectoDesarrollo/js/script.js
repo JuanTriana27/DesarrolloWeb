5245d1f15a425ab54e9347826f1e1f2ecebe52d3
JuanTriana27 <126177178+JuanTriana27@users.noreply.github.com>
2024-03-15 19:56:04 -0500

    Add files via upload

---
+++ ProyectoDesarrollo/js/script.js
@@ -0,0 +1,12 @@
+ document.getElementById('crear').addEventListener('click', function() {
+    window.location.href = 'src/crearsala.html';
+  });
+  
+  document.getElementById('lista').addEventListener('click', function() {
+    window.location.href = 'src/listadesalas.html';
+  });
+  
+  document.getElementById('unirse').addEventListener('click', function() {
+    window.location.href = 'src/unirse.html';
+  });
+  
