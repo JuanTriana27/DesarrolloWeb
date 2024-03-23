831da3c419c4d4734de83237724dba8fc8116da5
JuanTriana27 <126177178+JuanTriana27@users.noreply.github.com>
2024-03-22 21:49:52 -0500

    Add files via upload

--- ProyectoDesarrollo/js/juego.js
+++ ProyectoDesarrollo/js/juego.js
@@ -5,10 +5,16 @@ window.onload = function() {
     let isDrawing = false;
     let lastX = 0;
     let lastY = 0;
+    let timerInterval;
+    let score = 0; //Almacenar puntos
 
     canvas.addEventListener('mousedown', (event) => {
         isDrawing = true;
         [lastX, lastY] = [event.offsetX, event.offsetY];
+        
+        if (!timerInterval) {
+            timerInterval = setInterval(updateTimer, 1000);
+        }
     });
 
     canvas.addEventListener('mousemove', (event) => {
@@ -34,4 +40,64 @@ window.onload = function() {
     document.getElementById('backButton').addEventListener('click', () => {
         window.location.href = "../index.html";
     });
+
+    const chatForm = document.getElementById('chatForm');
+    const chatMessages = document.getElementById('chatMessages');
+    const messageInput = document.getElementById('messageInput');
+
+    chatForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        const message = messageInput.value.trim();
+        if (message !== '') {
+            sendMessage(message);
+            messageInput.value = '';
+        }
+    });
+
+    function sendMessage(message) {
+        const messageDiv = document.createElement('div');
+        messageDiv.classList.add('chat-message');
+        messageDiv.textContent = message;
+        chatMessages.appendChild(messageDiv);
+        chatMessages.scrollTop = chatMessages.scrollHeight;
+
+        // Verificar si el mensaje coincide con la palabra actual
+        if (message.toLowerCase() === document.getElementById('randomWord').textContent.toLowerCase()) {
+            score += 20; // Sumar 20 puntos
+            alert('¡Ganaste 20 puntos!');
+            console.log('Score:', score);
+        }
+    }
+
+    function getRandomWord() {
+        const words = ['Gato', 'Perro', 'Casa', 'Carro', 'Moto', 'Sol', 'Luna', 'Computador'];
+        return words[Math.floor(Math.random() * words.length)];
+    }
+
+    // Función para actualizar la palabra actual
+function updateRandomWord() {
+    currentWord = getRandomWord();
+    const randomWordElement = document.getElementById('randomWord');
+    randomWordElement.textContent = currentWord;
+    randomWordElement.classList.add('chat-word'); // Agregar la clase
+}
+
+
+
+    function updateTimer() {
+        const timerElement = document.getElementById('timer');
+        let timer = parseInt(timerElement.textContent);
+        if (timer === 0) {
+            clearInterval(timerInterval);
+            timer = 60;
+            updateRandomWord();
+            timerInterval = setInterval(updateTimer, 1000);
+            alert("¡Tiempo agotado!");
+        } else {
+            timer--;
+        }
+        timerElement.textContent = timer;
+    }
+
+    updateRandomWord();
 }
